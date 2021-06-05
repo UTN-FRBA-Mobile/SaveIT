816fa9378d980edda867888a7f2740574bb73abf
Santiago Varela <santiagovarela.86@gmail.com>
2021-06-05 17:18:22 -0300

    couple of dropdown

--- app/src/main/java/com/example/saveit/ui/reportes/ReportesDateChartFragment.kt
+++ app/src/main/java/com/example/saveit/ui/reportes/ReportesDateChartFragment.kt
@@ -16,6 +16,9 @@ class ReportesDateChartFragment: Fragment()  {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         _binding = ReportesDateChartFragmentBinding.inflate(inflater, container, false)
+
+
+
         return binding.root
     }
 
