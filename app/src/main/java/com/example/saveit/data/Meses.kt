6a042d104679933d3aa6abfbf7c89d882db5db23
martinsamo <martin-samo@hotmail.com>
2021-06-06 20:04:26 -0300

    Grafico de torta

---
+++ app/src/main/java/com/example/saveit/data/Meses.kt
@@ -0,0 +1,16 @@
+package com.example.saveit.data
+enum class Meses(val valor: Int, val descripcion: String) {
+    ENERO(1, "Enero"),
+    FEBRERO(2, "Febrero"),
+    MARZO(3, "Marzo"),
+    ABRIL(4, "Abril"),
+    MAYO(5, "Mayo"),
+    JUNIO(6, "Junio"),
+    JULIO(7, "Julio"),
+    AGOSTO(8, "Agosto"),
+    SEPTIEMBRE(9, "Septiembre"),
+    OCTUBRE(10, "Octubre"),
+    NOVIEMBRE(11, "Noviembre"),
+    DICIEMBRE(12, "Diciembre")
+
+}
