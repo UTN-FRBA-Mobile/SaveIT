6568c7ccd7ccd59bbc6cd8a513438fb35d9faa47
martinsamo <martin-samo@hotmail.com>
2021-06-21 11:39:23 -0300

    Un poco de google maps

--- app/src/main/java/com/example/saveit/ui/reportes/ReportesMapChartFragment.kt
+++ app/src/main/java/com/example/saveit/ui/reportes/ReportesMapChartFragment.kt
@@ -4,23 +4,93 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.loader.app.LoaderManager
+import com.example.saveit.R
 import com.example.saveit.databinding.ReportesMapChartFragmentBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.reportes_fragment.view.*
+import kotlinx.android.synthetic.main.reportes_map_chart_fragment.*
 
-class ReportesMapChartFragment : Fragment() {
+
+
+
+   /* override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reportes_map_chart_fragment)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}*/
+
+
+class ReportesMapChartFragment : Fragment(), OnMapReadyCallback {
     private var _binding: ReportesMapChartFragmentBinding? = null
+    private lateinit var mMap: GoogleMap
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+
         _binding = ReportesMapChartFragmentBinding.inflate(inflater, container, false)
+        createMapFragment()
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun createMapFragment() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.myGoogleMap) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
 
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+
+        val coordinates = LatLng(-34.605810144384535, -58.43575288699898)
+        val marker = MarkerOptions().position(coordinates).title("Titulo")
+        mMap.addMarker(marker)
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates,18f),
+            4000,
+            null
+        )
+    }
+
 }
